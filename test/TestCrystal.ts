290765a700947cd337776e83926950c7348be286
Christian Felde <cfelde@cfelde.com>
2022-03-30 21:19:48 +0100

    Add missing tests

--- test/TestCrystal.ts
+++ test/TestCrystal.ts
@@ -19,7 +19,7 @@
 import { expect } from "chai";
 import { ethers } from "hardhat";
 import { BigNumber} from "ethers";
-import { Crystal } from "../typechain-types";
+import { Crystal, TestBorrower } from "../typechain-types";
 
 describe("Crystal contract", function () {
     it("Deployment should start with default values", async function () {
@@ -47,5 +47,41 @@ describe("Crystal contract", function () {
         expect(await contract.lastChallenger()).to.equal("0x0000000000000000000000000000000000000000");
     });
 
-    // TODO Test flashFee, cap
+    it("Max total mint can be found on the cap function", async function () {
+        const Crystal = await ethers.getContractFactory("Crystal");
+        const contract = (await Crystal.deploy(
+            "NuPoW Quartz",
+            "NPQ",
+            "0x000000000000000000000000000000000000000000115eec47f6cf7e35000000"
+        )) as Crystal;
+
+        expect(await contract.MAX_TOTAL_MINT()).to.be.equal(BigNumber.from("0x000000000000000000000000000000000000000000115eec47f6cf7e35000000"));
+        expect(await contract.cap()).to.be.equal(BigNumber.from("0x000000000000000000000000000000000000000000115eec47f6cf7e35000000"));
+        expect(await contract.totalSupply()).to.be.equal(BigNumber.from("0"));
+    });
+
+    it("Flash fee equals nextMint", async function () {
+        const Crystal = await ethers.getContractFactory("Crystal");
+        const contract = (await Crystal.deploy(
+            "NuPoW Quartz",
+            "NPQ",
+            "0xffff000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000000"
+        )) as Crystal;
+
+        const Borrower = await ethers.getContractFactory("TestBorrower");
+        const borrower = (await Borrower.deploy()) as TestBorrower;
+
+        expect(await contract.MAX_MINT()).to.equal(BigNumber.from("0x1000000000000000"));
+        expect(await contract.nextMint()).to.equal(BigNumber.from("0x1000000000000000"));
+        expect(await contract.flashFee(contract.address, "0xffff000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000000")).to.equal(BigNumber.from("0x1000000000000000"));
+        expect(await contract.flashFee(contract.address, "0x000000000000000000000000000000000000000000115eec47f6cf7e35000000")).to.equal(BigNumber.from("0x1000000000000000"));
+        expect(await contract.maxFlashLoan(contract.address)).to.equal(BigNumber.from("0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"));
+
+        await contract.mint("0x3283f3d25b89965fb75c6ac6f643668ac7a7f6eeecfc215388e2c4e28f632b8b", "");
+        await contract.transfer(borrower.address, "0x1000000000000000");
+
+        expect(await borrower.counter()).to.be.equal(BigNumber.from("0"));
+        await contract.flashLoan(borrower.address, contract.address, "0xffff000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000cfe1de000000", [1, 2, 3]);
+        expect(await borrower.counter()).to.be.equal(BigNumber.from("1"));
+    });
 });
